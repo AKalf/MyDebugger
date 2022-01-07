8821e57a6d4e0c78d0226576a05854b6e27ad390
Alex_Kalf <alexkalfoglou@outlook.com.gr>
2022-01-07 16:09:59 +0200

    More stable, on unity launch and better handling for DB, - Appropriate UNITY_EDITOR macros to work with builds

---
+++ Editor/StorageInspector_MyDebugger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using MyDebugger.Elements;
+namespace MyDebugger.Storage.EditorScripts {
+    [CustomEditor(typeof(Storage_MyDebugger))]
+    public class StorageInspector_MyDebugger : Editor {
+
+        public override void OnInspectorGUI() {
+            serializedObject.Update();
+            if (Storage_MyDebugger.Instance.Root == null)
+                return;
+            EditorGUILayout.LabelField(Storage_MyDebugger.Instance.Root.CleanName + ", files: " + Storage_MyDebugger.Instance.Root.Files.Count);
+            foreach (LogFolder_Element_MyDebugger folder in Storage_MyDebugger.Instance.Root.GetChildFolders()) {
+                EditorGUILayout.LabelField(folder.CleanName + ", files: " + folder.Files.Count);
+                DrawFolders(folder.GetChildFolders());
+            }
+        }
+        private void DrawFolders(LogFolder_Element_MyDebugger[] folders) {
+            foreach (LogFolder_Element_MyDebugger folder in folders) {
+                EditorGUILayout.LabelField(folder.CleanName + ", files: " + folder.Files.Count);
+                DrawFolders(folder.GetChildFolders());
+            }
+
+        }
+    }
+
+
+}
